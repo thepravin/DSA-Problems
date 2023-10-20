b425650fe05d589b26f50abe3bd03297c3c668dc
PravinNalawade <thepravin28@gmail.com>
2023-10-20 18:13:41 +0530

    insert-AT-Tails

---
+++ LinkedList/Basic/insert-At-Tails.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertedAtTails(Node*&head, Node *&tail, int data)
+{
+        if (head == NULL)
+        {
+            Node*newNode = new Node(data);
+            head = newNode;
+            tail = newNode;
+            return;
+
+        }
+        
+    // Step 1 : create newNode object
+    Node *newNode = new Node(data);
+
+    // Step 2 : connect tail with new node
+    tail->next = newNode;
+
+    // Step 3 : update tail
+    tail = newNode;
+    
+}
+
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)  {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    // insertAtHead(head,tail,20);
+    // insertAtHead(head,tail,10);
+    insertedAtTails(head,tail,30);
+    insertedAtTails(head,tail,40);
+
+    print(head);
+
+    return 0;
+}

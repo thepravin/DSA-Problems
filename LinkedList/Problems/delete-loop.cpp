c44565ccb72334a3f355b316f693c0e3f8dfac0a
PravinNalawade <thepravin28@gmail.com>
2023-10-25 21:48:56 +0530

    delete loop added

--- LinkedList/Problems/delete-loop.cpp
+++ LinkedList/Problems/delete-loop.cpp
@@ -29,51 +29,44 @@ int getLength(Node *&head)
 }
 
 // ************************************************
-Node*removeLoop(Node*&head){
-    if (head==NULL)
+Node *removeLoop(Node *&head)
+{
+    if (head == NULL)
     {
         return head;
     }
 
-    Node* slow = head;
-    Node*fast = head;
-    while (fast!=NULL)
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL)
     {
         fast = fast->next;
-        if (fast!=NULL)
+        if (fast != NULL)
         {
-            fast=fast->next;
-            slow=slow->next;
+            fast = fast->next;
+            slow = slow->next;
         }
 
         if (slow == fast)
         {
-            // they meet then 
-            slow= head;
+            // they meet then
+            slow = head;
             break;
         }
-        
-        
     }
 
     // crete one previous pointer behind the fast pointer
-    Node* prev = fast;
-   while (slow!=fast)
-   {
-    prev=fast;
-    slow=slow->next;
-    fast=fast->next;
-   }
-   
-   prev->next=NULL;
-   return slow;
-    
-    
+    Node *prev = fast;
+    while (slow != fast)
+    {
+        prev = fast;
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    prev->next = NULL;
+    return slow;
 }
-
-
-
-
 
 // *********************************************
 
@@ -95,29 +88,23 @@ int main()
     Node *third = new Node(30);
     Node *fourth = new Node(40);
     Node *fifth = new Node(50);
-   Node *sixth = new Node(60);
-   Node *seventh = new Node(70);
-   Node *eighth = new Node(80);
-   Node *ninth= new Node(90);
+    Node *sixth = new Node(60);
+    Node *seventh = new Node(70);
+    Node *eighth = new Node(80);
+    Node *ninth = new Node(90);
 
     head->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = fifth;
     fifth->next = sixth;
-   sixth->next=seventh;
-   seventh->next = eighth;
-   eighth->next =ninth;
-   ninth->next =eighth;
+    sixth->next = seventh;
+    seventh->next = eighth;
+    eighth->next = ninth;
+    ninth->next = eighth;
 
- removeLoop(head);
- print(head);
-  
-
-
-
-  
-  
+    removeLoop(head);
+    print(head);
 
     return 0;
 }

c90b56b5887fd22856f8da30170bd7b3b2941c9b
Pravin Nalawade <pravinonline05@gmail.com>
2024-01-02 11:13:09 +0530

    Approach 2 : LeetCode-138

--- LinkedList/Problems/138-copyWithRandomPointer.cpp
+++ LinkedList/Problems/138-copyWithRandomPointer.cpp
@@ -1,7 +1,9 @@
 // LeetCode - 138 Copy with Random Pointr LL
-
+#include<iostream>
+using namespace std;
 
 // Approach 1
+/*
 
     Node* helper(Node*head,unordered_map<Node*,Node*>&mp){
         if(head==0) return head;
@@ -28,3 +30,58 @@
         return helper(head,mp);
 
     }
+
+    */
+
+
+/*
+
+
+class Solution {
+public:
+    // Approach 2 - Without map
+
+    Node* helper(Node*head){
+        if(!head) return head;
+
+        // Step 1 : Clone oldNode -> newNode
+        Node* it = head; //iterate old head 
+        while(it){
+            Node* cloneNode = new Node(it->val);
+            cloneNode->next = it->next;
+            it->next = cloneNode;
+            it=it->next->next;
+        }
+
+        // Step 2 : Assign random links 
+        it = head;
+        while(it){
+            Node*cloneNode = it->next;
+            cloneNode->random = it->random ? it->random->next : nullptr;
+            it=it->next->next;
+        }
+
+        // Step 3 : Detach
+        it=head;
+        Node* cloneHead = it->next;
+        while(it){
+            Node* cloneNode = it->next;
+            it->next = it->next->next;
+            if(cloneNode->next){
+                cloneNode->next = cloneNode->next->next;
+            }
+            it=it->next;
+        }
+        return cloneHead;
+
+    }
+
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+
+        return helper(head);
+    }
+};
+
+
+*/

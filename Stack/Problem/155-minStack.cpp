631ee12a867efcefe7da647ea5ff39232e0765c9
Pravin Nalawade <pravinonline05@gmail.com>
2024-01-08 17:09:23 +0530

    LeetCode 155 - updated

--- Stack/Problem/155-minStack.cpp
+++ Stack/Problem/155-minStack.cpp
@@ -1,1 +1,38 @@
 // Leetcode 155 : Min stack
+
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+class MinStack {
+public:
+    vector<pair<int, int>> st;
+
+    MinStack() {}
+
+    void push(int val) {
+        if (st.empty()) {
+            // stack was empty
+            // first value is min value
+            pair<int, int> p = make_pair(val, val);
+            st.push_back(p);
+        } else {
+            pair<int, int> p;
+            p.first = val;
+            p.second = min(val, st.back().second);
+            st.push_back(p);
+        }
+    }
+
+    void pop() { st.pop_back(); }
+
+    int top() {
+        return st.back().first;
+
+    }
+
+    int getMin() {
+        return st.back().second;
+    }
+};

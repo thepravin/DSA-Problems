01c3a4f315a634e6c565f7003111f9946949bdb6
Pravin Nalawade <thepravin28@gmail.com>
2024-05-28 15:21:33 +0530

    heap insertion

---
+++ Heaps/Basic/creation.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+
+class Heap
+{
+public:
+    int arr[101];
+    int size;
+
+    Heap()
+    {
+        size = 0;
+    }
+
+    void insert(int value)
+    {
+
+        // 1. add value at end of array
+        size = size + 1;
+        int index = size;
+        arr[index] = value;
+
+        // 2. iss value ko sahi jagah pr set karo
+        while (index > 1) // array start from 1 index
+        {
+            int parentIndex = index / 2;
+            if (arr[index] > arr[parentIndex])
+            {
+                swap(arr[index], arr[parentIndex]);
+                index = parentIndex;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+};
+
+int main(){
+    Heap h;
+    h.arr[0]=-1;
+    h.arr[1]=100;
+    h.arr[2]=50;
+    h.arr[3]=60;
+    h.arr[4]=40;
+    h.arr[5]=45;
+
+    h.size= 5;
+
+    cout<<"Printing the heap : "<<endl;
+    for(int i=0;i<=h.size;i++){
+        cout<<h.arr[i]<<" ";
+    }
+
+    // insert
+    h.insert(110);
+
+    cout<<endl<<"After Insertion :"<<endl;
+     for(int i=0;i<=h.size;i++){
+        cout<<h.arr[i]<<" ";
+    }
+}

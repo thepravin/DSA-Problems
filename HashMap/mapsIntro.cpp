3684741ddb10fd3509035465160ddfce7076145c
Pravin Nalawade <thepravin28@gmail.com>
2024-06-27 20:16:51 +0530

    map intro

--- HashMap/mapsIntro.cpp
+++ HashMap/mapsIntro.cpp
@@ -10,7 +10,7 @@ int main()
     // creation
     unordered_map<string, int> m;
 
-// insertion
+//----------------->>>> insertion <<<---------------
     // 1
     pair<string, int> p = make_pair("pravin", 3);
     m.insert(p);
@@ -21,11 +21,62 @@ int main()
 
     // 3
     m["city"] = 2;
-    cout<<m["city"]<<endl;
+    // cout<<m["city"]<<endl;
 
     // what will happen ???
     m["city"] = 5; // if key is present then update value
-    cout<<m["city"]<<endl;
+    // cout<<m["city"]<<endl;
+
+
+//----------------->>>> Search <<<---------------
+    // cout<<m["pravin"]<<endl;
+    // cout<<m.at("pravin")<<endl;
+
+    // Important for MCQ
+    // Type 1 
+        //cout<<m.at("unknownKey")<<endl; // o/p => give out of range error
+    // Type 2
+        // cout<<m["unknownKey"]<<endl;  // first find if not present then make new entry with 0 , o/t=> 0
+        // cout<<m.at("unknownKey")<<endl; // o/t => 0
+
+
+//----------------->>>> Size <<<---------------
+// cout<<"Size : "<<m.size()<<endl;
+
+
+//----------------->>>> check present of not <<<---------------
+    // if present then 1 , if not then 0
+    // cout<<m.count("pravin")<<endl; // o/p=> 1
+    // cout<<m.count("present")<<endl; // o/p => 0
+
+//----------------->>>> Erase <<<---------------
+// cout<<"Size : "<<m.size()<<endl;
+// m.erase("pravin");
+// cout<<"Size : "<<m.size()<<endl;
+
+
+//----------------->>>> print <<<---------------
+// Type 1 ==> using for loop
+/*
+for(auto i:m){
+    cout<<i.first<<":"<<i.second<<endl;
+}
+*/
+
+// Type 2 ==> using iterator
+/*
+unordered_map<string,int>:: iterator it = m.begin();
+
+while (it != m.end())
+{
+    cout<<it->first<<":"<<it->second<<endl;
+    it++;
+}
+*/
+
+
+
+
 
 
     return 0;

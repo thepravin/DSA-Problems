b81b4bafd765ffbf455e25f5e24ba9ed5ff695e5
Pravin Nalawade <thepravin28@gmail.com>
2025-11-07 21:05:20 +0530

    LeetCode 238

---
+++ Array/Problems/238-Product_Of_Array_Except_Self.cpp
@@ -0,0 +1,55 @@
+
+/*
+----------------------------------------------------------------------
+ Approach : 1 (my devision)
+ T.C = O( n )
+ S.C = O( 1 )
+
+1. take product of all elements of array
+2. divide product for each number and store division into ans array.
+
+Note : avoide zero
+----------------------------------------------------------------------
+*/
+
+
+
+/*
+----------------------------------------------------------------------
+ Approach : 2 (previous elements product * next all elements product)
+ T.C = O( 3n )
+ S.C = O( 2n ) --> use dynamic array using 'new'
+----------------------------------------------------------------------
+*/
+
+ vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n,0);
+        vector<int> prefixProduct(n, 0);
+        vector<int> suffixProduct(n, 0);
+
+        prefixProduct[0] = nums[0];
+        suffixProduct[n - 1] = nums[n - 1];
+
+        // prefix product 
+        for (int i = 1; i < n; ++i) {
+            prefixProduct[i] = prefixProduct[i - 1] * nums[i];
+        }
+
+        // suffix product
+        for (int i = n - 2; i >= 0; --i) {
+            suffixProduct[i] = suffixProduct[i + 1] * nums[i];
+        }
+
+
+        ans[0] = suffixProduct[1];
+        for (int i = 1; i < n - 1; i++) 
+        {
+            ans[i] = prefixProduct[i - 1] * suffixProduct[i + 1];
+        }
+
+        ans[n - 1] = prefixProduct[n - 2];
+
+        return ans;
+    }
